fc0fd3cce337498dab1ecf398d6cf34b6e5ffe91
ylyao <yyl537110@163.com>
2018-12-23 13:30:02 +0800

    '项目初始化'

---
+++ ng-client/src/main/java/com/slj/NgClientApplication.java
@@ -0,0 +1,14 @@
+package com.slj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgClientApplication.class, args);
+	}
+
+}
+
